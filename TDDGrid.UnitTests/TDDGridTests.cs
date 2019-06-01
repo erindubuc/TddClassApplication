622fbbf31ca53c8b784f87934e546042363fcf88
erin.dubuc9@gmail.com <erin.dubuc9@gmail.com>
2019-06-01 14:29:04 -0400

    Created tests for checking cells around active/alive cells

--- TDDGrid.UnitTests/TDDGridTests.cs
+++ TDDGrid.UnitTests/TDDGridTests.cs
@@ -31,7 +31,7 @@ namespace TDDGrid.UnitTests
 
             Assert.AreEqual(expected, actual);
         }
-        
+
         [Test]
         public void IfStateOfCellIsFalse_SwitchToTrue()
         {
@@ -43,7 +43,7 @@ namespace TDDGrid.UnitTests
             Assert.AreEqual(expected, actual);
         }
 
-        
+
         [Test]
         public void IfStateOfCellIsTrue_SwitchToFalse()
         {
@@ -56,5 +56,120 @@ namespace TDDGrid.UnitTests
 
             Assert.AreEqual(expected, actual);
         }
+
+        /*  For a space that is 'populated':
+                Each cell with one or no neighbors dies, as if by solitude.
+                Each cell with four or more neighbors dies, as if by overpopulation.
+                Each cell with two or three neighbors survives.
+            For a space that is 'empty' or 'unpopulated'
+                Each cell with three neighbors becomes populated.
+        */
+      
+
+        [Test]
+        public void CheckActiveCellForHorizontalNeighbors_ReturnHorizontalCountOfNeighbors()
+        {
+            _gameBoard.SwitchStateOfCell(3, 5);
+            _gameBoard.SwitchStateOfCell(3, 6);
+            _gameBoard.SwitchStateOfCell(3, 4);
+            var expected = 2;
+            var actual = _gameBoard.CountAllHorizontalNeighborsOfActiveCell(3, 5);
+            Assert.AreEqual(expected, actual);
+        }
+        
+        [Test]
+        public void CheckActiveCellForVerticalNeighbors_ReturnVerticalCountOfNeighbors()
+        {
+            _gameBoard.SwitchStateOfCell(3, 5);
+            _gameBoard.SwitchStateOfCell(2, 5);
+            var expected = 1;
+            var actual = _gameBoard.CountAllVerticalNeighborsOfActiveCell(3, 5);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void CheckActiveCellForLeftDiagonalNeighbors_ReturnCountOfNeighbors()
+        {
+            _gameBoard.SwitchStateOfCell(3, 5);
+            _gameBoard.SwitchStateOfCell(2, 4);
+            _gameBoard.SwitchStateOfCell(4, 6);
+            var expected = 2;
+            var actual = _gameBoard.CountAllDiagonalNeighborsOfActiveCell(3, 5);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void CheckActiveCellForRightDiagonalNeighbors_ReturnCountOfNeighbors()
+        {
+            _gameBoard.SwitchStateOfCell(3, 5);
+            _gameBoard.SwitchStateOfCell(2, 6);
+            _gameBoard.SwitchStateOfCell(4, 4);
+            var expected = 2;
+            var actual = _gameBoard.CountAllDiagonalNeighborsOfActiveCell(3, 5);
+            Assert.AreEqual(expected, actual);
+        }
+        /*
+        [Test]
+        public void CheckActiveCellForAnyActiveNeighbors_ReturnCountOfNeighbors()
+        {
+            _gameBoard.SwitchStateOfCell(3, 5);
+            _gameBoard.SwitchStateOfCell(2, 6);
+            _gameBoard.SwitchStateOfCell(4, 4);
+            _gameBoard.SwitchStateOfCell(2, 4);
+            _gameBoard.SwitchStateOfCell(4, 6);
+            _gameBoard.SwitchStateOfCell(3, 6);
+            _gameBoard.SwitchStateOfCell(3, 4);
+            _gameBoard.SwitchStateOfCell(2, 5);
+            var expected = 7;
+            var actual = _gameBoard.CountActiveNeighborsOfActiveCell(3, 5);
+            Assert.AreEqual(expected, actual);
+        }
+
+
+
+
+        [Test]
+        public void GetCountOfActiveNeighborsOfActiveCellOnEdgeOfBoard_ReturnCount()
+        {
+            _gameBoard.SwitchStateOfCell(6, 0);
+            _gameBoard.SwitchStateOfCell(5, 1);
+            
+            var expected = 1;
+            var actual = _gameBoard.CountActiveNeighborsOfActiveCell(6, 0);
+            Assert.AreEqual(expected, actual);
+        }
+        
+        [Test]
+        public void CheckBoardForOneActiveCell_ReturnIndexOfActiveCell()
+        {
+            _gameBoard.SwitchStateOfCell(3, 5);
+            
+            List<int> returnedBoard = new List<int> { 3, 5 };
+
+            var expected = returnedBoard;
+            var actual = _gameBoard.GetAllActiveCellsInBoard();
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        
+        [Test]
+        public void CheckBoardForActiveCells_ReturnArrayOfActiveCells()
+        {
+            _gameBoard.SwitchStateOfCell(6, 5);
+            _gameBoard.SwitchStateOfCell(2, 10);
+            _gameBoard.SwitchStateOfCell(13, 7);
+
+
+            List<int> returnedBoard = new List<int>();
+
+            var expected = returnedBoard;
+            var actual = _gameBoard.GetAllActiveCellsInBoard();
+            Assert.AreEqual(expected, actual);
+
+        }
+        */
+
+
     }
 }
--- TDDGrid/GameBoard.cs
+++ TDDGrid/GameBoard.cs
@@ -8,10 +8,12 @@ namespace TDDGrid
 {
     public class GameBoard 
     {
-        private const int Rows = 10;
-        private const int Columns = 10;
+        public const int Rows = 15;
+        public const int Columns = 15;
         public bool[,] boardState = new bool[Rows,Columns];
         public bool isCellChecked = false;
+        public int neighborCount = 0;
+        //public List<int> activeCells = new List<int>(); 
 
         public GameBoard()
         {
@@ -33,13 +35,210 @@ namespace TDDGrid
         public bool SwitchStateOfCell(int row, int col)
         {
             if (boardState[row, col] == isCellChecked)
-                return isCellChecked = true;
+                boardState[row,col] = true;
             else
-                return isCellChecked;
+                boardState[row,col] = isCellChecked;
+            return boardState[row, col];
         }
 
-       
+        public int CountAllHorizontalNeighborsOfActiveCell(int row, int col)
+        {
+            int horizontalNeighborCount = 0;
+            if (boardState[row, col] == true)
+            {
+                if (col == 0)
+                {
+                    // horz
+                    if (boardState[row, col + 1] == true)
+                        horizontalNeighborCount++;
+                }
+                else if(col == GameBoard.Columns - 1)
+                {
+                    // horz
+                    if (boardState[row, col - 1] == true)
+                        horizontalNeighborCount++;
+                }
+                else
+                {
+                    // horz
+                    if (boardState[row, col - 1] == true)
+                        horizontalNeighborCount++;
+                    if (boardState[row, col + 1] == true)
+                        horizontalNeighborCount++;
+                }
+            }
+            return horizontalNeighborCount;
+        }
 
+        public int CountAllVerticalNeighborsOfActiveCell(int row, int col)
+        {
+            int verticalNeighborCount = 0;
+            if (boardState[row, col] == true)
+            {
+                if (row == 0)
+                {
+                    if (boardState[row + 1, col] == true)
+                        neighborCount++;
+                }
+                else if (row == GameBoard.Rows - 1)
+                {
+                    if (boardState[row - 1, col] == true)
+                        verticalNeighborCount++;
+                }
+                else
+                {
+                    // vert
+                    if (boardState[row - 1, col] == true)
+                        verticalNeighborCount++;
+                    if (boardState[row + 1, col] == true)
+                        verticalNeighborCount++;
+
+                }
+            }
+            return verticalNeighborCount;
+        }
+
+        public int CountAllDiagonalNeighborsOfActiveCell(int row, int col)
+        {
+            int diagonalNeighborCount = 0;
+            if (boardState[row, col] == true)
+            {
+                if (col == 0)
+                {
+                    if (row == 0)
+                    {
+                        if (boardState[row + 1, col + 1] == true)
+                            diagonalNeighborCount++;
+                    }
+
+                    if (row == GameBoard.Rows - 1)
+                    {
+                        if (boardState[row - 1, col + 1] == true)
+                            diagonalNeighborCount++;
+                    }
+
+                    if (boardState[row + 1, col + 1] == true)
+                        diagonalNeighborCount++;
+
+                    if (boardState[row - 1, col + 1] == true)
+                        diagonalNeighborCount++;
+
+                }
+                else if (col == GameBoard.Columns - 1)
+                {
+                    if (row == 0)
+                    {
+                        if (boardState[row + 1, col - 1] == true)
+                            diagonalNeighborCount++;
+                    }
+                    if (row == GameBoard.Rows - 1)
+                    {
+                        if (boardState[row - 1, col - 1] == true)
+                            diagonalNeighborCount++;
+                    }
+
+                    if (boardState[row + 1, col - 1] == true)
+                        diagonalNeighborCount++;
+
+                    if (boardState[row - 1, col - 1] == true)
+                        diagonalNeighborCount++;
+                }
+                /*
+                else if (row == 0)
+                {
+                    if ()
+                }
+                else if (row == GameBoard.Rows - 1)
+                {
+                }
+                */
+            }
+            else
+            {
+                // diag left
+                if (boardState[row - 1, col - 1] == true)
+                    diagonalNeighborCount++;
+                if (boardState[row + 1, col + 1] == true)
+                    diagonalNeighborCount++;
+                // diag right
+                if (boardState[row - 1, col + 1] == true)
+                    diagonalNeighborCount++;
+                if (boardState[row + 1, col - 1] == true)
+                    diagonalNeighborCount++;
+            }
+
+            return diagonalNeighborCount;
+        }
+        public int CountActiveNeighborsOfActiveCell(int row, int col)
+        {
+            int neighborCount = 0;
+            if (boardState[row, col] == true)
+            {
+                if (col == 0)
+                {
+                    // vert
+                    if (boardState[row - 1, col] == true)
+                        neighborCount++;
+                    if (boardState[row + 1, col] == true)
+                        neighborCount++;
+                    // horz
+                    if (boardState[row, col + 1] == true)
+                        neighborCount++;
+                    // diag left
+                    if (boardState[row + 1, col + 1] == true)
+                        neighborCount++;
+                    // diag right
+                    if (boardState[row - 1, col + 1] == true)
+                        neighborCount++;
+
+
+
+
+                }
+                else
+                {
+                    // vert
+                    if (boardState[row - 1, col] == true)
+                        neighborCount++;
+                    if (boardState[row + 1, col] == true)
+                        neighborCount++;
+                    // horz
+                    if (boardState[row, col - 1] == true)
+                        neighborCount++;
+                    if (boardState[row, col + 1] == true)
+                        neighborCount++;
+                    // diag left
+                    if (boardState[row - 1, col - 1] == true)
+                        neighborCount++;
+                    if (boardState[row + 1, col + 1] == true)
+                        neighborCount++;
+                    // diag right
+                    if (boardState[row - 1, col + 1] == true)
+                        neighborCount++;
+                    if (boardState[row + 1, col - 1] == true)
+                        neighborCount++;
+                }
+                
+            }
+            return neighborCount;
+        }
+        /*
+        public List<int> GetAllActiveCellsInBoard()
+        {
+            for (int row = 0; row < GameBoard.Rows; row++)
+            {
+                for (int col = 0; col < GameBoard.Columns; col++)
+                {
+                    if (GetStateOfSpecificCell(row,col) == true)
+                    {
+                        activeCells.Add(row);
+                        activeCells.Add(col);
+                    }
+                }
+            }
+            return activeCells;
+        }
+        */
         public void DrawBoard()
         {
             for (int row = 0; row < Rows; row++)
