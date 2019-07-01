5d6fcd946f08a4faee8128495d6b165f1cde7412
erin.dubuc9@gmail.com <erin.dubuc9@gmail.com>
2019-07-01 14:00:37 -0400

    Returns expanded form of number as string from long num input

---
+++ ExpandedForm/ExpandedForm_FromGivenNumber/Expander.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpandedForm_FromGivenNumber
+{
+    public class Expander
+    {
+        public static string GetExpandedFormOfNumber(long number)
+        {
+            string numberStr = number.ToString();
+
+            StringBuilder expandedForm = new StringBuilder();
+
+            int numberOfZeros = numberStr.Length - 1;
+
+            for (int index = 0; index < numberStr.Length - 1; index++)
+            {
+                if (numberStr[index] == '0')
+                    continue;
+
+                if (index != numberStr.Length - 1)
+                    expandedForm.Append(numberStr[index]).Append('0', numberOfZeros - index).Append(" + ");
+
+            }
+
+            if (numberStr[numberStr.Length - 1] != '0')
+                expandedForm.Append(numberStr[numberStr.Length - 1]);
+
+            else
+            {
+                for (int lengthOfExtraSpace = 0; lengthOfExtraSpace <= 2; lengthOfExtraSpace++)
+                    expandedForm.Length--;
+            }
+
+            return expandedForm.ToString();
+        }
+
+    }
+        
+}
---
+++ ExpandedForm/ExpandedForm_FromGivenNumber_Tests/ExpanderTests.cs
@@ -0,0 +1,67 @@
+﻿using NUnit.Framework;
+using ExpandedForm_FromGivenNumber;
+
+namespace ExpandedForm_FromGivenNumber_Tests
+{
+    [TestFixture]
+    public class ExpanderTests
+    {
+        [Test]
+        public void IfLengthIsOne_ReturnNumber()
+        {
+            long number = 1;
+            string expected = "1";
+            string actual = Expander.GetExpandedFormOfNumber(number);
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void IfLengthIsTwo_ReturnTensPlusOnes()
+        {
+            long number = 21;
+            string expected = "20 + 1";
+            string actual = Expander.GetExpandedFormOfNumber(number);
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void IfLengthIsTwoAndOnePlaceIsZero_ReturnTensOnly()
+        {
+            long number = 20;
+            string expected = "20";
+            string actual = Expander.GetExpandedFormOfNumber(number);
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void IfLengthIsThreeAndThereAreNoZeros_ReturnHundredsTensAndOnes()
+        {
+            long number = 375;
+            string expected = "300 + 70 + 5";
+            string actual = Expander.GetExpandedFormOfNumber(number);
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void IfLengthIsMoreThanThreeAndNoZeros_ReturnHundredsTensAndOnes()
+        {
+            long number = 54213;
+            string expected = "50000 + 4000 + 200 + 10 + 3";
+            string actual = Expander.GetExpandedFormOfNumber(number);
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void IfLengthIsMoreThanThreeAndHasZeros_ReturnHundredsTensAndOnes()
+        {
+            long number = 54203;
+            string expected = "50000 + 4000 + 200 + 3";
+            string actual = Expander.GetExpandedFormOfNumber(number);
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void IfOnlyFirstDigitIsANumberNotEqualToZero_ReturnExpectedOutput()
+        {
+            long number = 9000000;
+            string expected = "9000000";
+            string actual = Expander.GetExpandedFormOfNumber(number);
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
